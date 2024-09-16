fdc28fb45581ec1a41437d1e048c9be37c12d5b0
EUNSEMIN <dmstpals104@ajou.ac.kr>
2024-09-16 16:36:55 +0900

    feat: Implement skill and monster system

--- Dungeon Jumak/Assets/00. Scripts/99. Data/00. Character/Player_Dunjeon/DunjeonPlayerData_Base.cs
+++ Dungeon Jumak/Assets/00. Scripts/99. Data/00. Character/Player_Dunjeon/DunjeonPlayerData_Base.cs
@@ -1,11 +1,16 @@
-﻿//Engine
+//Engine
 using UnityEngine;
 
 namespace Data.Character
 {
-    [CreateAssetMenu(fileName = "DunjeonPlayerData", menuName = "Scriptable/Character/Player/DunjeonPlayer")]
+    [CreateAssetMenu(fileName = "Dunjeon Player Data", menuName = "Scriptable/Character/Player/DunjeonPlayer")]
     public class DunjeonPlayerData_Base : ScriptableObject
     {
+        [Header("플레이어 정보")]
+        [Tooltip("플레이어의 던전에서의 최대 체력 수치를 설정합니다.")]
+        [SerializeField] private float m_hp;
+
+        public float Hp { get { return m_hp; } }
     }
 }
 
